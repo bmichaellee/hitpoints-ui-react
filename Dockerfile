02a5c97a0760187ac588d363257fcb10619b2671
B. Michael Lee <bmichaellee@outlook.com>
2022-01-24 14:57:01 -0500

    Add API

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM node:16 AS builder
+FROM node:16-alpine AS builder
 WORKDIR /ui
-COPY package*.json ./
-RUN npm i
-COPY ./ .
+COPY package*.json .
+RUN apk update \
+  && apk add git \
+  && npm i
+COPY . .
 RUN npm run build
 
 FROM nginx:alpine
--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { TestFunc } from 'hitpoints-api-client';
+
 function App() {
+  TestFunc()
+    .then( res => console.log( res ));
+
   return (
     <div className="App">
       <header className="App-header">
